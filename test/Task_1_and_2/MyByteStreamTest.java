1a717f9af3942e1145d1d74a872de45759886127
Beata Chu <beataslav@gmail.com>
2019-03-29 16:56:37 +0300

    Тесты MyByteStream
    Добавлены тесты в класс MyByteStreamTest

--- test/Task_1_and_2/MyByteStreamTest.java
+++ test/Task_1_and_2/MyByteStreamTest.java
@@ -1,16 +1,51 @@
 package Task_1_and_2;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static java.io.File.createTempFile;
 import static org.junit.Assert.*;
 
 public class MyByteStreamTest {
+    File tempFile;
+    MyByteStream byteStream;
+    String filePrefix = "inputFile";
+    String fileSuffix = ".tmp";
+    BufferedWriter bw;
+
+    @Before
+    public void setUp() throws Exception {
+        tempFile = createTempFile(filePrefix, fileSuffix);
+        byteStream = new MyByteStream();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        bw.close();
+        tempFile.deleteOnExit();
+    }
 
     @Test
-    public void readText() {
+    public void readText() throws Exception{
+        bw = new BufferedWriter(new FileWriter(tempFile));
+        String expectedString = "public static String readFileName throws IOException";
+        String[] expected = expectedString.split("\\s*(\\s|,|;|\\.)\\s*");
+        bw.write(expectedString);
+        bw.close();
+        assertArrayEquals(expected, byteStream.readText(tempFile.getAbsolutePath()));
     }
 
     @Test
     public void writeKeysToFile() {
+
+
     }
 }
